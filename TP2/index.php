b3e803c949ad89ee365d1f58e5e445221b4172cd
M1caela <micacalvoo@gmail.com>
2025-06-23 20:02:51 -0300

    se completa nav + footer (responsive)

--- TP2/index.php
+++ TP2/index.php
@@ -6,33 +6,65 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
 ?>
 
 <!DOCTYPE html>
-<html lang="es">
+<html lang="es" data-theme="light">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Habiffy</title>  
-    <link rel="stylesheet" href="../src/output.css">   
-    <link rel="stylesheet" href="css/styles.css">  
+  
+    <link rel="stylesheet" href="css/styles.css">    
+    <link rel="stylesheet" href="../src/output.css"> 
 </head>
 
-<body class="bg-verde-claro m-0" id="home">
+<!-- bg-verde-claro -->
+<body class="bg-base-200 bg-verde-claro" id="home">
 
     <!-- BANNER -->
-    <div class="navbar flex bg-verde-oscuro w-full h-auto"> 
+     <!-- bg-verde-oscuro  -->
+    <!-- <div class="bg-base-300 flex w-full h-auto "> 
         <div class="m-[12px]">
-            <h1 class="pb-2">Habiffy</h1>
+            <h1 class="pb-4">Habiffy</h1>
             <h2>la constancia se convierte en éxito</h2>
         </div>
     
-        <!--  MENÚ: pefil / salir --> 
-        <div class="flex align-top justify-end self-end"><a href="login.php" title="Cerrar sesión">
-            <a href="perfil.php" title="Perfil"><img src="img/mi-perfil.png" alt="Ícono de Mi Perfil" class="ml-2 hover:scale-110 transition cursor-pointer"></a>
-            <a href="login-php" title="Cerrar sesión"><img src="img/cerrar-sesion.png" alt="Ícono de Salir" class="ml-2 hover:scale-110 transition cursor-pointer"></a> </div>
+        < !--  MENÚ: pefil / salir -- > 
+        <div class="flex justify-end flex-row-reverse"><a href="login.php" title="Cerrar sesión">
+            <a href="perfil.php" title="Perfil"><img src="img/mi-perfil.png" alt="Ícono de Mi Perfil" class="ml-2 hover:scale-110 transition cursor-pointer "></a>
+            <a href="login.php" title="Cerrar sesión"><img src="img/cerrar-sesion.png" alt="Ícono de Salir" class="ml-2 hover:scale-110 transition cursor-pointer "></a>
+        </div>
+    </div> -->
+
+    <!-- componentr nav -->
+    <div class="navbar bg-base-300 bg-verde-oscuro shadow-sm">
+        <div class="flex-1 m-8">
+            <h1 class="text-xl pb-2">Habiffy</h1>
+            <h2>la constancia se convierte en éxito</h2>
+        </div>
+
+        <div class="flex-none m-4">
+            <!-- MENÚ -->
+            <div class="dropdown dropdown-end">
+                <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+                    <div class="w-10 rounded-full">
+                        <img alt="Menú" src="img/menu.png" /> 
+                    </div>
+                </div>
+
+                <!-- opciones -->
+                <ul tabindex="0"class="menu menu-sm dropdown-content bg-base-300 bg-verde-oscuro rounded-box z-1 mt-3 w-52 p-2 shadow">
+                  
+                    <li><a>Perfil</a></li>   
+                    <li><a>Preguntas</a></li> <!-- *falta desarrollar* -->
+                    <li><a>Contacto</a></li>
+                    <li><a>Cerrar sesión</a></li>
+                </ul>
+            </div>
         </div>
     </div>
 
-    <div class="flex justify-center text-center">
-        <h3 class="font-semibold text-xl">¡Hola de nuevo<?php $NombreUsuario ?>!🫡</h3>  
+    <!-- bienvenida -->
+    <div class="flex justify-center text-center ">
+        <h3 class="font-semibold text-xl p-4 m-2">¡Hola de nuevo<?php $NombreUsuario ?>!🫡</h3>  
     </div>
 
     <div id="contenedor" class="p-8 m-4 max-w-[98%]">    
@@ -129,7 +161,7 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
         </div>   
 
         <div class="subcontenedor flex justify-center align-items flex-col md:flex-row lg:flex-row p-4 max-w-[98%]"> 
-            <!-- CALENDARIO *por ahora es solo visual, más adelante se conecta con el seguimiento del hábito* -->
+            <!-- CALENDARIO -->
             <section class="calendario self-star">        
                 <calendar-date class="cally bg-base-100 border border-base-300 shadow-lg rounded-box">
                     <svg aria-label="Previous" class="fill-current size-4" slot="previous" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M15.75 19.5 8.25 12l7.5-7.5"></path></svg>
@@ -142,44 +174,16 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
                 <section class="notas" > </section> 
             -->
         </div>
-
-        <!-- PREGUNTAS FRECUENTES 
-        <div class="preguntas p-8 m-6 w-full md:w-[30%]">
-        <h2 class="text-bold text-lg md:text-xl">Preguntas frecuentes</h2>
-            <div class="collapse bg-beige border-300 m-8 max-w-[80%] md:max-w-[40%]">
-                <input type="checkbox" />
-                <div class="collapse-title font-semibold">¿Cómo funciona Habiffy?</div>
-                <div class="collapse-content text-sm">
-                texto texto 
-                </div>
-            </div>
-    -->        
-            <div class="collapse bg-beige border-base-300 border m-8 max-w-[80%] md:max-w-[40%]">
-                <input type="checkbox" />
-                <div class="collapse-title font-semibold">¿Pregunta?</div>
-                <div class="collapse-content text-sm">
-                texto texto 
-                </div>
-            </div>
-
-            <div class="collapse bg-beige border-base-300 border m-8 max-w-[80%] md:max-w-[40%]">
-                <input type="checkbox" />
-                <div class="collapse-title font-semibold">¿Pregunta?</div>
-                <div class="collapse-content text-sm">
-                texto texto 
-                </div>
-            </div>
-        </div>
     </div>
 
     <!-- CONTROLADOR DE TEMA -->
-    <div class="join join-vertical">
+    <div class="join join-horizontal m-10">
         <input
             type="radio"
             name="theme-buttons"
             class="btn bg-verde-oscuro theme-controller join-item"
-            aria-label="Default"
-            value="default" />
+            aria-label="Mix"
+            value="dark-light" />
         <input
             type="radio"
             name="theme-buttons"
@@ -195,10 +199,62 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
 
     </div>
 
-    <footer class="bg-verde-oscuro h-[50px] md:h-[80px] w-full">
+    <footer class="footer flex sm:footer-horizontal bg-verde-oscuro text-neutral-content items-center p-4 m-0">
+        <!-- logo -->
+        <aside class="grid-flow-col items-center">
+            <svg
+            width="36"
+            height="36"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            class="fill-current">
+            <path
+                d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z"></path>
+            </svg>
+            <p>Copyright © 2025 - Derechos Reservados</p>
+        </aside>
+
+        <!-- redes -->
+        <nav class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <a>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    class="fill-current">
+                    <path
+                    d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                </svg>
+            </a>
+            <a>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    class="fill-current">
+                    <path
+                    d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                </svg>
+            </a>
+            <a>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    class="fill-current">
+                    <path
+                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                </svg>
+            </a>
+        </nav>
     </footer>
 
-    <!-- confirm personalizado -->
+    <!-- confirmación personalizada (para eliminar habito por ej) -->
     <input type="checkbox" id="modal-alerta" class="modal-toggle" />
     <div class="modal" id="modalConfirm">
         <div class="modal-box">
@@ -211,18 +267,7 @@ date_default_timezone_set('America/Argentina/Buenos_Aires');
         </div>
     </div>
 
-    <!-- <script> 
-        function confirmarEliminacion(boton) {
-            var id = boton.getAttribute('data-id');
-            var habito = boton.getAttribute('data-habito');
-
-            if (confirm(`¿Eliminar el hábito "${habito}"? Esta acción no se puede deshacer.`)) {
-                window.location.href = "eliminar.php?id=" + id;
-            }
-        }
-    </script> -->
-
-    <!-- controlador checkbox -->
+    <!-- controlador de checkbox para 'completar' habito -->
     <script>
         function marcarCompletado(checkbox) {
             const id = checkbox.getAttribute('data-id');
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,34 +8,109 @@ module.exports = {
   },
 
   plugins: [require("daisyui")],
-  daisyui: {
-    themes: [
-      {
-        light: {
-          "color-scheme": "light",
-          "base-100": "#fcf9e7",
-          "base-200": "#d3dca1",
-          "base-300": "#1c6065",
-          "base-content": "#052628",
-          "primary": "#d3dca1",
-          "primary-content": "#1c6065",
-          "secondary": "#1c6065",
-          "secondary-content": "#d3dca1",
-          "accent": "#d8b16b",
-          "accent-content": "oklch(38% 0.063 188.416)",
-          "neutral": "#6D7D7E",
-          "neutral-content": "#052628",
-          "info": "#fcf9e7",
-          "info-content": "#1c6065",
-          "success": "#1c6065",
-          "success-content": "#d3dca1",
-          "warning": "oklch(26% 0.079 36.259)",
-          "warning-content": "oklch(83% 0.128 66.29)",
-          "error": "oklch(25% 0.092 26.042)",
-          "error-content": "oklch(88% 0.062 18.334)",
-        }
-      }
-    ]
-  }
 }
-
+  // daisyui: {
+  // themes: 
+  // [
+    // {
+//       light: {
+//         "color-scheme": "light", 
+//         "base-100": "#fcf9e7", 
+//         "base-200": "#d3dca1", 
+//         "base-300": "#1c6065",
+//         "base-content": "#052628",
+//         "primary": "#d3dca1",
+//         "primary-content": "#1c6065",
+//         "secondary": "#1c6065",
+//         "secondary-content": "#d3dca1",
+//         "accent": "#d8b16b",
+//         "accent-content": "oklch(38% 0.063 188.416)",
+//         "neutral": "#6D7D7E",
+//         "neutral-content": "#052628",
+//         "info": "#fcf9e7",
+//         "info-content": "#1c6065",
+//         "success": "#1c6065",
+//         "success-content": "#d3dca1",
+//         "warning": "oklch(26% 0.079 36.259)",
+//         "warning-content": "oklch(83% 0.128 66.29)",
+//         "error": "oklch(25% 0.092 26.042)",
+//         "error-content": "oklch(88% 0.062 18.334)",
+//         "--radius-selector": "0.5rem",
+//         "--radius-field": "0.25rem",
+//         "--radius-box": "0.5rem",
+//         "--size-selector": "0.25rem",
+//         "--size-field": "0.25rem",
+//         "--border": "1px",
+//         "--depth": "1",
+//         "--noise": "0",
+//       }
+//     },
+//     {
+//       dark: {
+//         "color-scheme": "dark",
+//         "base-100": "#124A4E",
+//         "base-200": "#052628",
+//         "base-300": "oklch(26% 0.051 172.552)",
+//         "base-content": "#C6DBCE",
+//         "primary": "#124A4E",
+//         "primary-content": "#052628",
+//         "secondary": "#B3CE8E",
+//         "secondary-content": "#052628",
+//         "accent": "#C49E59",
+//         "accent-content": "#052628",
+//         "neutral": "#AEB7B8",
+//         "neutral-content": "#052628",
+//         "info": "oklch(14% 0 0)",
+//         "info-content": "#B3CE8E",
+//         "success": "#C49E59",
+//         "success-content": "#052628",
+//         "warning": "#945D35",
+//         "warning-content": "oklch(95% 0.038 75.164)",
+//         "error": "#943535",
+//         "error-content": "oklch(88% 0.062 18.334)",
+//         "--radius-selector": "0.5rem",
+//         "--radius-field": "0.25rem",
+//         "--radius-box": "0.5rem",
+//         "--size-selector": "0.25rem",
+//         "--size-field": "0.25rem",
+//         "--border": "1px",
+//         "--depth": "1",
+//         "--noise": "0",
+//       }
+//     },
+//     {
+//       "dark-light": {
+//         "color-scheme": "light-dark",
+//         "base-100": "#d3dca1",
+//         "base-200": "#1c6065",
+//         "base-300": "oklch(95% 0 0)",
+//         "base-content": "oklch(21% 0.006 285.885)",
+//         "primary": "#d3dca1",
+//         "primary-content": "#1c6065",
+//         "secondary": "#fcf9e7",
+//         "secondary-content": "#1c6065",
+//         "accent": "#d8b16b",
+//         "accent-content": "oklch(38% 0.063 188.416)",
+//         "neutral": "#d8b16b",
+//         "neutral-content": "oklch(21% 0.006 285.885)",
+//         "info": "oklch(14% 0 0)",
+//         "info-content": "#D4E4E5",
+//         "success": "#d3dca1",
+//         "success-content": "oklch(21% 0.006 285.885)",
+//         "warning": "oklch(26% 0.079 36.259)",
+//         "warning-content": "oklch(55% 0.135 66.442)",
+//         "error": "oklch(25% 0.092 26.042)",
+//         "error-content": "oklch(87% 0.01 258.338)",
+//         "--radius-selector": "0.5rem",
+//         "--radius-field": "0.5rem",
+//         "--radius-box": "0.5rem",
+//         "--size-selector": "0.21875rem",
+//         "--size-field": "0.21875rem",
+//         "--border": "1px",
+//         "--depth": "0",
+//         "--noise": "1",
+//       }
+//     }
+//   ]
+// }
+// };
